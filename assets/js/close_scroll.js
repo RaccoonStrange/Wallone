09d96b485f8e32f3c03fdffa18744a7418117bd5
Kisus <strange_raccoon@mail.ru>
2022-09-15 18:18:42 +0300

    Страница профиль и стили к нему

--- assets/js/close_scroll.js
+++ assets/js/close_scroll.js
@@ -5,8 +5,16 @@ document.addEventListener("DOMContentLoaded", function(){
         document.body.style.overflow = 'hidden';
         document.querySelector('#openModal').style.marginLeft = scrollbar;
     });
+    document.querySelector('[href="#openModalFriends"]').addEventListener('click',function(){
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModalFriends').style.marginLeft = scrollbar;
+    });
     document.querySelector('[href="#close"]').addEventListener('click',function(){
         document.body.style.overflow = 'visible';
         document.querySelector('#openModal').style.marginLeft = '0px';
+    });
+    document.querySelector('[href="#close"]').addEventListener('click',function(){
+        document.body.style.overflow = 'visible';
+        document.querySelector('#openModalFriends').style.marginLeft = '0px';
     });
 });
